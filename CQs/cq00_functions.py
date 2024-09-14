4ff1c0b3585f5abd6047af1226b272263ee5bbcb
Vincent Maynard <vinzy@unc.edu>
2024-09-13 21:06:48 -0400

    Challenge Question 2

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,16 @@
+"""CQ00 Functions Practice"""
+
+__author__ = "730764770"
+
+
+def mimic(message: str) -> str:
+    """Return inputted message"""
+    return message
+
+
+def main() -> None:
+    return print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
